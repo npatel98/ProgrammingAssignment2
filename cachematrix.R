6c16f093b6f568513319f957fae3df9552df84bc
Nikhil Patel <nikhilpatel@Nikhils-MacBook-Pro.local>
2020-04-20 12:09:57 -0700

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -27,7 +27,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   i <- NULL
   set <- function(matrix) {
     x <<- matrix
